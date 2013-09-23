f240a7516ab53aad899e7c0c1f8775ae88e4df7c
valdec <eckhard@tactix4.com>
2013-09-23 12:23:37 +0100

    removed graph view, changes to project description, additional category

--- __openerp__.py
+++ __openerp__.py
@@ -26,20 +26,23 @@
     'website': 'http://www.tactix4.com',
     'category': 'Project Management',
     'description': """
-       This module allows to manage risk in at least two different contexts
-       Project Management
-       Business Continuity Planning
+       This module allows to manage risk in at least two different contexts:
+
+       1) Project Management
+
+       2) Business Continuity Planning
        
        A risk is always associated with a project and has an impact and probability assessment pre- and post-response with resulting expected values (scores).
-       Actions on risk are tasks, which can be identified easily in the extended task view via the 'Action on Risk' button and associated risk id.
+       Actions on risk are tasks, which can be identified easily in the extended task view (Projects > Tasks) via the 'Action on Risk' filter and associated risk id.
+       In addition, tasks which are actions on risks are coloured green in the task list (tree) view.
        
-       Risk categories, response categories and proximity are set-up according to the PRINCE2 project methodology.
-       They are easily changed via the 'Configuration' menu to get them in accordance with Business Continuity Planning, PIP, SCRUM or DSDM.
+       Risk categories, response categories and proximity are set-up according to the PRINCE2 project methodology as well as for Business Continuity Planning.
+       They are easily changed via the 'Configuration' menu to get them in accordance with PIP, SCRUM or DSDM.
        
        Besides the above mentioned information the following information can and ought to be documented as well:
        Risk description, author, date registered, date modified, event, effect, cause and risk owner.
        
-       A graphical view to get an overview of the risks based on the expected values (scores) is included as well.
+       Two user groups are added, which a user must be associated with to work with Risks, either Risk / User or Risk / Manager.
         
        More information can be found on our website: http://valuedecision.com
        
