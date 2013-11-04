b38cb0fd6a88b1b205779441e54a7a1c9e4ec86b
Les Green <lesgreen@tactix4.net>
2013-11-04 15:11:38 +0000

    Fixed test for checking the follower dictionary

--- tests/test_risks.py
+++ tests/test_risks.py
@@ -3,8 +3,8 @@ __author__ = 'valdec'
 from openerp.tests import common
 from datetime import datetime
 
-class TestRisks(common.TransactionCase):
 
+class TestRisks(common.TransactionCase):
     def setUp(self):
 
         """***setup risk tests***"""
@@ -16,20 +16,20 @@ class TestRisks(common.TransactionCase):
         self.task_model = self.registry('project.task')
         self.user_model = self.registry('res.users')
 
-        self.test_project_id = self.project_model.create(cr, uid,{
+        self.test_project_id = self.project_model.create(cr, uid, {
             'name': 'RiskTestProject'
         })
-        self.risk_owner_id = self.user_model.search(cr, uid, [('name','=','Demo User')])[0]
-        self.risk_author_id = self.user_model.search(cr, uid, [('name','=','Risk User')])[0]
-        self.risk_second_author_id = self.user_model.search(cr, uid,[('login','=','riskmanager')])[0]
+        self.risk_owner_id = self.user_model.search(cr, uid, [('name', '=', 'Demo User')])[0]
+        self.risk_author_id = self.user_model.search(cr, uid, [('name', '=', 'Risk User')])[0]
+        self.risk_second_author_id = self.user_model.search(cr, uid, [('login', '=', 'riskmanager')])[0]
 
         self.test_risk_id = self.risk_model.create(cr, uid, {
-             'name': 'RiskTest0001',
-             'description': 'TestRisk_SkyFallsIn',
-             'risk_category_id': 1,
-             'project_id': self.test_project_id,
-             'author_id': self.risk_author_id,
-             'risk_owner_id': self.risk_owner_id
+            'name': 'RiskTest0001',
+            'description': 'TestRisk_SkyFallsIn',
+            'risk_category_id': 1,
+            'project_id': self.test_project_id,
+            'author_id': self.risk_author_id,
+            'risk_owner_id': self.risk_owner_id
         })
 
     def test_risk_owner_and_creator_added_to_followers_for_risk(self):
@@ -60,35 +60,33 @@ class TestRisks(common.TransactionCase):
         self.risk_model.write(cr, uid, [self.test_risk_id],
                               {'risk_response_ids': [
                                   [0, False,
-                                    {'remaining_hours': 0,
-                                     'priority': '2',
-                                     'stage_id': 1,
-                                     'planned_hours': 0,
-                                     'user_id': uid,
-                                     'name': 'My New Task',
-                                     'date_deadline': False,
-                                     'sequence': 10,
-                                     'date_end': False,
-                                     'date_start': False,
-                                     'child_ids': [[6, False, []]],
-                                     'company_id': 1,
-                                     'work_ids': [],
-                                     'parent_ids': [[6, False, []]],
-                                     'message_follower_ids': followers,
-                                     'categ_ids': [[6, False, []]],
-                                     'project_id': 1,
-                                     'partner_id': False,
-                                     'message_ids': False,
-                                     'description': 'A new Task'}]]
+                                   {'remaining_hours': 0,
+                                    'priority': '2',
+                                    'stage_id': 1,
+                                    'planned_hours': 0,
+                                    'user_id': uid,
+                                    'name': 'My New Task',
+                                    'date_deadline': False,
+                                    'sequence': 10,
+                                    'date_end': False,
+                                    'date_start': False,
+                                    'child_ids': [[6, False, []]],
+                                    'company_id': 1,
+                                    'work_ids': [],
+                                    'parent_ids': [[6, False, []]],
+                                    'message_follower_ids': followers,
+                                    'categ_ids': [[6, False, []]],
+                                    'project_id': 1,
+                                    'partner_id': False,
+                                    'message_ids': False,
+                                    'description': 'A new Task'}]]
                               })
 
-
         task_ids = self.risk_model.read(cr, uid, self.test_risk_id, ['risk_response_ids'])
-        tasks = self.task_model.read(cr, uid, task_ids['risk_response_ids'],['message_follower_ids'])
+        tasks = self.task_model.read(cr, uid, task_ids['risk_response_ids'], ['message_follower_ids'])
         for task in tasks:
-            self.assertEqual(followers, task['message_follower_ids'],msg='Followers are not set on the associated action')
-
-
-
-
+            self.assertEqual(len(followers), len(task['message_follower_ids']),
+                             msg='Incorrect number of followers set on the associated action')
+            for follower in followers:
+                self.assertIn(follower, task['message_follower_ids'], msg='Follower missing from follower_ids')
 
